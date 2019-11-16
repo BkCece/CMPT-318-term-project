7126b9c456ed45e99bd577a1b7ee135690ccc685
pna18 <pna18@sfu.ca>
2019-11-16 14:26:20 -0800

    finished the python script to plot

--- phase1.py
+++ phase1.py
@@ -5,16 +5,16 @@ import sys
 train_data = pd.read_csv(sys.argv[1], sep = ',')
 test_data = pd.read_csv(sys.argv[2], sep = ',')
 
-train_agg = train_data.groupby(['day', 'hour', 'week'])['Global_active_power', 'Voltage', 'Global_reactive_power', 'Global_intensity'].agg(['mean', 'min', 'max'])
+train_agg = train_data.groupby(['day', 'hour', 'week'])['Global_active_power', 'Voltage', 'Global_reactive_power', 'Global_intensity'].agg(['mean', 'min', 'max']).reset_index()
 
 train_agg = train_agg.sort_values(by = ['week'])
-
-test_agg = test_data.groupby(['day', 'hour', 'week'])['Global_active_power', 'Voltage', 'Global_reactive_power', 'Global_intensity'].agg(['mean', 'min', 'max'])
+test_agg = test_data.groupby(['day', 'hour', 'week'])['Global_active_power', 'Voltage', 'Global_reactive_power', 'Global_intensity'].agg(['mean', 'min', 'max']).reset_index()
 
 test_agg = test_agg.sort_values(by = ['week'])
 
-plt.figure(figsize=(10, 5))
+plt.figure(figsize=(20, 5))
 plt.subplot(1, 2, 1)
-plt.plot(train_data['week'], train_agg['Global_active_power']['mean'].groupby(['week']), 'b-')
+plt.scatter(train_agg['week'], train_agg['Global_active_power']['mean'], marker='o')
 plt.subplot(1, 2, 2)
-plt.plot(test_data['week'], test_agg['Global_active_power']['mean'].groupby(['week']), 'r-')
+plt.scatter(test_agg['week'], test_agg['Global_active_power']['mean'], marker='o')
+plt.show()
